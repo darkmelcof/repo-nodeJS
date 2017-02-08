0ea05517c8feaf278157cb25e91afad04ee38588
Clément FONTAINE <darkmelcof@gmail.com>
2017-02-08 23:38:13 +0100

    add Files

---
+++ mymodule.js
@@ -0,0 +1,10 @@
+var sayHello = function() {
+    console.log('Hello !');
+}
+
+var sayGoodBye = function() {
+    console.log('Good bye !');
+}
+
+exports.sayHello = sayHello;
+exports.sayGoodBye = sayGoodBye;
